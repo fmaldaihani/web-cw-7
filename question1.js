25905d6f35e4d8b3eaa05b536227a5c30a65114e
f <fmaldaihani@gmail.com>
2021-01-05 20:57:27 +0300

    r

--- question1.js
+++ question1.js
@@ -1,1 +1,21 @@
 // قم بحل السؤال الأول هنا
+let pets= ["rabbit","turtle","bird","cats","dogs"]
+
+
+pets.pop(pets[4])
+console.log(pets)
+
+
+let A= prompt("whats your favorite pets?");
+
+pets.push(A)
+
+
+let cats = {
+    name: "lulu"
+    age: "2 years"
+    type: "cats"
+    emoji: "🐹"
+
+}
+console.log(cats.age);
